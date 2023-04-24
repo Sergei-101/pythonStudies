6b904cda4372402ad7c0c8e1e1753acfd265f2e1
Сергей <Sergei@mail.ru>
2023-04-24 21:44:03 +0300

    задание 8 дополнительная

--- HomeWork.py
+++ HomeWork.py
@@ -1,8 +1,7 @@
-num = input('Введите номер билетика: ')
-res = [int(x) for x in str(num)]
-res_one = int(res[0])+int(res[1])+int(res[2])
-res_two = int(res[3])+int(res[4])+int(res[5])
-if res_one==res_two:
-    print('Поздравляем, у вас счастливый билетик')
+n = int(input('Введите кол-во долек n: '))
+m = int(input('Введите кол-во долек m: '))
+k = int(input('Сколько долек вы хотите отломить k: '))
+if (n-1)>=k or (m-1)>=k:
+    print('Да')
 else:
-    print('Может в другой раз повезет')
+    print("Нет")
