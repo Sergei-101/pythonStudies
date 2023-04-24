c5ce09fd86fd81f172bb394a5d7a87526d6e98c5
Сергей <Sergei@mail.ru>
2023-04-24 21:31:25 +0300

    задание 4

--- HomeWork.py
+++ HomeWork.py
@@ -1,4 +1,10 @@
-a = input('Введите целое положительное число ')
-b = a+a
-c = a+a+a
-print(int(a)+int(b)+int(c))
+enter_revenue = int(input('Введите выручку фирмы '))
+enter_costs = int(input('Введите издержки фирмы '))
+if enter_revenue>enter_costs:
+    employees = int(input('Введите численность сотрудников'))
+    fin_resylt = enter_revenue-enter_costs
+    rent = fin_resylt/enter_revenue
+    profit_emp = fin_resylt/employees
+    print(f'Фирма отработала с прибылью = {fin_resylt}. Рентабильность выручки = {rent}, прибыль фирмы в расчете на одного сотрудника {profit_emp}')
+else:
+    print("Фирма отработала в убыток")
