0ce9b17e1de7a5324ddbd56c9d52b38648e54db7
Сергей <Sergei@mail.ru>
2023-04-24 21:38:24 +0300

    задание 2 дополнительная

--- HomeWork.py
+++ HomeWork.py
@@ -1,10 +1,10 @@
-enter_revenue = int(input('Введите выручку фирмы '))
-enter_costs = int(input('Введите издержки фирмы '))
-if enter_revenue>enter_costs:
-    employees = int(input('Введите численность сотрудников'))
-    fin_resylt = enter_revenue-enter_costs
-    rent = fin_resylt/enter_revenue
-    profit_emp = fin_resylt/employees
-    print(f'Фирма отработала с прибылью = {fin_resylt}. Рентабильность выручки = {rent}, прибыль фирмы в расчете на одного сотрудника {profit_emp}')
+n=int(input('Введите трехзначное число '))
+if 999>=n>=100:
+    sum = 0
+    while n>0:
+        z = n%10
+        sum = z+sum
+        n//=10
+    print(sum)
 else:
-    print("Фирма отработала в убыток")
+    print('Я же просил трехзначное')
