5f67ccd91670599f5057a0437f9ed01cf94d6e24
Сергей <Sergei@mail.ru>
2023-04-24 21:42:01 +0300

    задание 6 дополнительная

--- HomeWork.py
+++ HomeWork.py
@@ -1,3 +1,8 @@
-s = int(input('Введите кол-во журавликов: '))
-x = int(s/3/2)
-print(f'Петя {x}, Катя {x*4}, Сережа {x} ')
+num = input('Введите номер билетика: ')
+res = [int(x) for x in str(num)]
+res_one = int(res[0])+int(res[1])+int(res[2])
+res_two = int(res[3])+int(res[4])+int(res[5])
+if res_one==res_two:
+    print('Поздравляем, у вас счастливый билетик')
+else:
+    print('Может в другой раз повезет')
